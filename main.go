7251b2b64165b182afb8effd9d44086c128f509f
artkescha <work.interin@yandex.ru>
2023-01-02 17:48:57 +0300

    fix user interactive

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	var wantResult int
+	fmt.Println("Enter want result:")
 	fmt.Scanf("%d\n", &wantResult)
 	fmt.Printf("iteration metod stepsCount: %d\n", iteration(wantResult))
 	fmt.Printf("recursive metod stepsCount: %d\n", recursive(wantResult))
